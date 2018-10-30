d7cd64e4b8ac3e1ba5006d402afea0f1b025aea1
Matthew Speake <matthew.speake@gmail.com>
2018-10-30 09:47:03 +0000

    Finished first pass of terraform configuration and setup Jenkins pipeline etc.

---
+++ infrastructure/main.tf
@@ -0,0 +1,134 @@
+locals {
+  app_full_name = "${var.product}-${var.component}"
+  ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+  shared_vault_name = "${var.shared_product_name}-${local.local_env}"
+  s2s_vault_url = "https://s2s-${local.local_env}.vault.azure.net/"
+}
+# "${local.ase_name}"
+# "${local.app_full_name}"
+# "${local.local_env}"
+
+module "app" {
+  source = "git@github.com:hmcts/cnp-module-webapp?ref=master"
+  product = "${local.app_full_name}"
+  location = "${var.location}"
+  env = "${var.env}"
+  ilbIp = "${var.ilbIp}"
+  subscription = "${var.subscription}"
+  capacity     = "${var.capacity}"
+  is_frontend = false
+  additional_host_name = "${local.app_full_name}-${var.env}.service.${var.env}.platform.hmcts.net"
+  https_only="false"
+  common_tags  = "${var.common_tags}"
+  asp_rg = "${var.shared_product_name}-${var.env}"
+  asp_name = "${var.shared_product_name}-${var.env}"
+
+  app_settings = {
+    POSTGRES_HOST = "${module.db.host_name}"
+    POSTGRES_PORT = "${module.db.postgresql_listen_port}"
+    POSTGRES_DATABASE = "${module.db.postgresql_database}"
+    POSTGRES_USER = "${module.db.user_name}"
+    POSTGRES_PASSWORD = "${module.db.postgresql_password}"
+    MAX_ACTIVE_DB_CONNECTIONS = 5
+
+    # JAVA_OPTS = "${var.java_opts}"
+    # SERVER_PORT = "8080"
+
+    # db
+    SPRING_DATASOURCE_URL = "jdbc:postgresql://${module.db.host_name}:${module.db.postgresql_listen_port}/${module.db.postgresql_database}?ssl=true"
+    SPRING_DATASOURCE_USERNAME = "${module.db.user_name}"
+    SPRING_DATASOURCE_PASSWORD = "${module.db.postgresql_password}"
+
+    ENABLE_DB_MIGRATE="false"
+
+    # idam
+    #IDAM_API_BASE_URI = "${var.idam_api_url}"
+    #S2S_BASE_URI = "http://${var.s2s_url}-${local.local_env}.service.core-compute-${local.local_env}.internal"
+    #S2S_URL = "http://${var.s2s_url}-${local.local_env}.service.core-compute-${local.local_env}.internal"
+    #S2S_TOKEN = "${data.azurerm_key_vault_secret.s2s_secret.value}"
+
+    # logging vars & healthcheck
+    REFORM_SERVICE_NAME = "${local.app_full_name}"
+    REFORM_TEAM = "${var.team_name}"
+    REFORM_SERVICE_TYPE = "${var.app_language}"
+    REFORM_ENVIRONMENT = "${var.env}"
+
+    PACKAGES_NAME = "${local.app_full_name}"
+    PACKAGES_PROJECT = "${var.team_name}"
+    PACKAGES_ENVIRONMENT = "${var.env}"
+
+    ROOT_APPENDER = "${var.root_appender}"
+    JSON_CONSOLE_PRETTY_PRINT = "${var.json_console_pretty_print}"
+    LOG_OUTPUT = "${var.log_output}"
+  }
+}
+
+module "db" {
+  source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product = "${local.app_full_name}-postgres-db"
+  location = "${var.location}"
+  env = "${var.env}"
+  postgresql_user = "${var.postgresql_user}"
+  database_name = "${var.database_name}"
+  sku_name = "GP_Gen5_2"
+  sku_tier = "GeneralPurpose"
+  storage_mb = "51200"
+  common_tags  = "${var.common_tags}"
+}
+
+
+//data "azurerm_key_vault" "shared_key_vault" {
+//  name = "${local.shared_vault_name}"
+//  resource_group_name = "${local.shared_vault_name}"
+//}
+
+//data "azurerm_key_vault_secret" "s2s_secret" {
+//  name = "microservicekey-rpa-prd-org"
+//  vault_uri = "${local.s2s_vault_url}"
+//}
+
+//data "azurerm_key_vault_secret" "oauth2_secret" {
+//  name = "prd-org-oauth2-token"
+//  vault_uri = "${data.azurerm_key_vault.shared_key_vault.vault_uri}"
+//}
+
+//module "local_key_vault" {
+//  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+//  product = "${local.app_full_name}"
+//  env = "${var.env}"
+//  tenant_id = "${var.tenant_id}"
+//  object_id = "${var.jenkins_AAD_objectId}"
+//  resource_group_name = "${module.app.resource_group_name}"
+//  product_group_object_id = "5d9cd025-a293-4b97-a0e5-6f43efce02c0"
+//}
+
+//resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+//  name = "${local.app_full_name}-POSTGRES-USER"
+//  value = "${module.db.user_name}"
+//  vault_uri = "${module.local_key_vault.key_vault_uri}"
+//}
+
+//resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+//  name = "${local.app_full_name}-POSTGRES-PASS"
+//  value = "${module.db.postgresql_password}"
+//  vault_uri = "${module.local_key_vault.key_vault_uri}"
+//}
+
+//resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+//  name = "${local.app_full_name}-POSTGRES-HOST"
+//  value = "${module.db.host_name}"
+//  vault_uri = "${module.local_key_vault.key_vault_uri}"
+//}
+
+//resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+//  name = "${local.app_full_name}-POSTGRES-PORT"
+//  value = "${module.db.postgresql_listen_port}"
+//  vault_uri = "${module.local_key_vault.key_vault_uri}"
+//}
+
+//resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+//  name = "${local.app_full_name}-POSTGRES-DATABASE"
+//  value = "${module.db.postgresql_database}"
+//  vault_uri = "${module.local_key_vault.key_vault_uri}"
+//}
---
+++ infrastructure/output.tf
@@ -0,0 +1,26 @@
+// used for db migrations
+output "microserviceName" {
+  value = "${local.app_full_name}"
+}
+
+// used for db migrations
+//output "vaultName" {
+//  value = "${module.local_key_vault.key_vault_name}"
+//}
+
+// used for grabing shared secrects (shown in the jenkins file)
+//output "vaultUri" {
+//  value = "${data.azurerm_key_vault.shared_key_vault.vault_uri}"
+//}
+
+//output "idam_api_url" {
+//  value = "http://${var.idam_api_url}-${local.local_env}.service.core-compute-${local.local_env}.internal"
+//}
+
+//output "s2s_url" {
+//  value = "http://${var.s2s_url}-${local.local_env}.service.core-compute-${local.local_env}.internal"
+//}
+
+//output "test_s2s_token" {
+//  value = "${data.azurerm_key_vault_secret.source_test_s2s_secret.value}"
+//}
