ea00ed09850b8caeb49c45c3d0210a6707a81e21
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2018-11-28 12:40:56 +0000

    Add AppInsights code and configuration

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,9 +1,12 @@
 import grails.rest.render.xml.*
 
 import prd.organisation.domain.Organisation
+import prd.organisation.config.AppInsights
 
 beans = {
     organisationRenderer(XmlRenderer, Organisation) {
         includes = ['primaryContactFirstName', 'primaryContactEmail']
+    }
+    appInsights() {
     }
 }
---
+++ src/main/groovy/prd/organisation/config/AppInsights.groovy
@@ -0,0 +1,12 @@
+package prd.organisation.config
+
+import com.microsoft.applicationinsights.TelemetryClient
+import org.springframework.stereotype.Component
+import uk.gov.hmcts.reform.logging.appinsights.AbstractAppInsights
+
+@Component
+class AppInsights extends AbstractAppInsights {
+    AppInsights(TelemetryClient telemetry) {
+        super(telemetry)
+    }
+}
