d11316555b9d4e61888283f2977d8b2b477979d6
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2018-12-14 14:30:27 +0000

    Fix random infinite loop by adding Java 8 support plugin
    It turns out that the infinite loop was because Grails didn't know how to handle Java 8's new Date/Time objects.

--- grails-app/controllers/rd/professional/web/OrganisationController.groovy
+++ grails-app/controllers/rd/professional/web/OrganisationController.groovy
@@ -1,25 +1,16 @@
 package rd.professional.web
 
-import grails.converters.JSON
 import rd.professional.domain.Organisation
 import rd.professional.service.OrganisationService
 
 class OrganisationController extends SubclassRestfulController<Organisation> {
     static responseFormats = ['json', 'xml']
+
     OrganisationController() {
         super(Organisation)
     }
 
     OrganisationService organisationService
-
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        def orgs = listAllResources(params)
-        // Needed so that the custom marshalling is used, else we get an infinite loop
-        JSON.use('deep') {
-            render orgs as JSON
-        }
-    }
 
     def register(OrganisationRegistrationCommand cmd) {
         log.info "Creating organisation"
--- grails-app/init/rd/professional/BootStrap.groovy
+++ grails-app/init/rd/professional/BootStrap.groovy
@@ -1,62 +1,8 @@
 package rd.professional
 
-import grails.converters.JSON
-import rd.professional.domain.ContactInformation
-import rd.professional.domain.Domain
-import rd.professional.domain.Organisation
-import rd.professional.domain.PaymentAccount
-import rd.professional.domain.ProfessionalUser
-
 class BootStrap {
 
     def init = { servletContext ->
-        JSON.registerObjectMarshaller(Organisation, {
-            return [
-                    name:it.name,
-                    url:it.url,
-                    sraId:it.sraId,
-                    organisationId:it.organisationId,
-                    lastUpdated:it.lastUpdated,
-                    companyNumber:it.companyNumber,
-                    status:it.status,
-                    users:it.users,
-                    contacts:it.contacts,
-                    accounts:it.accounts,
-                    domains:it.domains,
-                    contacts:it.contacts
-            ]
-        })
-        JSON.registerObjectMarshaller(ProfessionalUser, {
-            return [
-                    userId:it.userId,
-                    emailId:it.emailId,
-                    firstName:it.firstName,
-                    lastName:it.lastName,
-                    status:it.status,
-                    contacts:it.contacts,
-                    accounts:it.accounts
-            ]
-        })
-        JSON.registerObjectMarshaller(ContactInformation, {
-            return [
-                    address:it.address,
-                    dateCreated:it.dateCreated,
-                    lastUpdated:it.lastUpdated,
-                    contactId:it.contactId
-            ]
-        })
-        JSON.registerObjectMarshaller(PaymentAccount, {
-            return [
-                    paymentAccountId:it.paymentAccountId,
-                    pbaNumber:it.pbaNumber
-            ]
-        })
-        JSON.registerObjectMarshaller(Domain, {
-            return [
-                    domainId:it.domainId,
-                    host:it.host,
-            ]
-        })
     }
     def destroy = {
     }
