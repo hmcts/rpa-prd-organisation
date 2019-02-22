d7ad02d12bdf6c4ad8e3900bf933295c251fad38
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2019-02-22 17:54:19 +0000

    Add functional tests for /search/(organisations|users)/ endpoints

--- src/integration-test/groovy/rd/professional/SearchControllerFunctionalSpec.groovy
+++ src/integration-test/groovy/rd/professional/SearchControllerFunctionalSpec.groovy
@@ -85,6 +85,70 @@ class SearchControllerFunctionalSpec extends GebSpec {
         resp.status == NOT_FOUND.value()
     }
 
+    void "Test search for user by email"() {
+        when: "a search request is made using the email address"
+        def resp = restBuilder().get("${baseUrl}search/users/foo@barsearch.com", {
+            accept("application/json")
+        })
+
+        then: "the relevant user is returned"
+        resp.status == OK.value()
+        def user = resp.json
+        user && user.firstName == "Foo"
+    }
+
+    void "Test search for user by email that's not in the database"() {
+        when: "a search request is made using the wrong email address"
+        def resp = restBuilder().get("${baseUrl}search/users/baz@qux.com", {
+            accept("application/json")
+        })
+
+        then: "an error is returned"
+        resp.status == NOT_FOUND.value()
+    }
+
+    void "Test search for user by email with no email given"() {
+        when: "a search request is made using no email address"
+        def resp = restBuilder().get("${baseUrl}search/users", {
+            accept("application/json")
+        })
+
+        then: "an error is returned"
+        resp.status == NOT_FOUND.value()
+    }
+
+    void "Test search for organisations by email"() {
+        when: "a search request is made using the email address"
+        def resp = restBuilder().get("${baseUrl}search/organisations/foo@barsearch.com", {
+            accept("application/json")
+        })
+
+        then: "the relevant organisation is returned"
+        resp.status == OK.value()
+        def org = resp.json
+        org && org.name == "Search Inc."
+    }
+
+    void "Test search for organisations by email that's not in the database"() {
+        when: "a search request is made using the wrong email address"
+        def resp = restBuilder().get("${baseUrl}search/organisations/baz@qux.com", {
+            accept("application/json")
+        })
+
+        then: "an error is returned"
+        resp.status == NOT_FOUND.value()
+    }
+
+    void "Test search for organisations by email with no email given"() {
+        when: "a search request is made using no email address"
+        def resp = restBuilder().get("${baseUrl}search/organisations", {
+            accept("application/json")
+        })
+
+        then: "an error is returned"
+        resp.status == NOT_FOUND.value()
+    }
+
     RestBuilder restBuilder() {
         new RestBuilder()
     }
